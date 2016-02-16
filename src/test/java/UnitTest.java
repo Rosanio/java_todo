f7a42e5220c43a79ae2d72740f71c07217dc8916
Matt Rosanio and Ramon de la Cruz <virgilioramon.delacruz@gmail.com>
2016-02-16 08:24:45 -0800

    add initial files and passed first two tests

---
+++ src/test/java/UnitTest.java
@@ -0,0 +1,27 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class UnitTest {
+
+  @Test
+  public void Task_instantiatesCorrectly_true() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals(true, myTask instanceof Task);
+  }
+
+  @Test
+  public void task_instantiatesWithDescription_true() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals("Mow the lawn", myTask.getDescription());
+  }
+
+
+/*
+@Test
+public void giveChange_25cents_1quarter() {
+  App testApp = new App();
+  assertEquals("{Nickels=0, Dimes=0, Quarters=1, Pennies=0}", testApp.giveChange(25).toString());
+}
+*/
+
+}
